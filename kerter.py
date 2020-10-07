660625f0cfbfdbf4b50e5c82fb3c39bf8ebad849
Gergosz-2000 <gergoszivak@gmail.com>
2020-10-07 10:38:11 +0200

    2.2

--- kerter.py
+++ kerter.py
@@ -10,13 +10,13 @@ if (szam = 4):
 		return 4*a*r
 	print(nyolcszogKerulet())
 	print(nyolcszogTerulet())
-if else (szam = 2):
+elif (szam = 2):
 	print("Mennyi a kör sugara?")
 	sugar = float(input())
 
 	print("Kerület =", 2 * sugar * 3.14,"cm")
 	print("Terület =", sugar**2 * 3.14,"cm^2")
-if else (szam = 1):
+elif(szam = 1):
 	def haromszogKerulet ():
 		a=float(input("Kérem a háromszög egyik odlalát[cm]: "))
 		b=float(input("Kérem a háromszög másik oldalát[cm]: "))
