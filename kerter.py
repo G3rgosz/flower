be3d444eb1d3690a91015354652f7b30138dc30b
Gergosz-2000 <gergoszivak@gmail.com>
2020-10-07 10:42:55 +0200

    működik!!!!!

--- kerter.py
+++ kerter.py
@@ -1,6 +1,6 @@
 print("1-háromszög, 2-kör, 3-téglalap, 4-nyolcszög")
 szam = int(input("Kérem írja be a választott művelet számát: "))
-if (szam = 4):
+if (szam == 4):
 	def nyolcszogKerulet():
 		a=float(input("Kérlek add meg a nyolcszög oldalát [cm]: "))
 		return 8*a
@@ -10,13 +10,13 @@ if (szam = 4):
 		return 4*a*r
 	print(nyolcszogKerulet())
 	print(nyolcszogTerulet())
-elif (szam = 2):
+elif (szam == 2):
 	print("Mennyi a kör sugara?")
 	sugar = float(input())
 
 	print("Kerület =", 2 * sugar * 3.14,"cm")
 	print("Terület =", sugar**2 * 3.14,"cm^2")
-elif(szam = 1):
+elif(szam == 1):
 	def haromszogKerulet ():
 		a=float(input("Kérem a háromszög egyik odlalát[cm]: "))
 		b=float(input("Kérem a háromszög másik oldalát[cm]: "))
@@ -26,7 +26,7 @@ elif(szam = 1):
 		return float(a+b+c)
 	print(teglalapTerulet())
 else:
-	print(nincs ilyen lehetőség)
+	print("nincs ilyen lehetőség")
 
 """
 Tesztelés
