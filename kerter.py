6a7c72cbe519324878e60c475a74aa97f6187b24
nagy0adam <nagyadam2092c@gmail.com>
2020-10-07 10:18:34 +0200

    haromszog ker

--- kerter.py
+++ kerter.py
@@ -1,5 +1,14 @@
 print("1-háromszög, 2-kör, 3-téglalap, 4-nyolcszög")
 szam = int(input("Kérem írja be a választott művelet számát: ")
+
+	haromszogKerulet ():
+	a=float(input("Kérem a háromszög egyik odlalát[cm]: "))
+	b=float(input("Kérem a háromszög másik oldalát[cm]: "))
+	c=float(input("Kérem a háromszög harmadik oldalát[cm]: "))
+	return float(a+b+c)
+	print(teglalapTerulet())
+
+
 def nyolcszogKerulet():
 		a=float(input("Kérlek add meg a nyolcszög oldalát [cm]: "))
 		return 8*a
@@ -8,7 +17,12 @@ def nyolcszogTerulet():
 		r=float(input("Kérlek add meg a nyolcszög sugarát [cm]: "))
 		return 4*a*r
 
-
+def haromszogKerulet ():
+	a=float(input("Kérem a háromszög egyik odlalát[cm]: "))
+	b=float(input("Kérem a háromszög másik oldalát[cm]: "))
+	c=float(input("Kérem a háromszög harmadik oldalát[cm]: "))
+	return float(a+b+c)
+	print(teglalapTerulet())
 """
 Tesztelés
 print(nyolcszogTerulet())
