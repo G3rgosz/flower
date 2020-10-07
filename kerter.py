9f7d52a91144e139e487a02c4ad3ed08de5fa343
Szoftver Szilard <szoftteszt2020@gmail.com>
2020-10-07 10:03:29 +0200

    8 szog terulet

--- kerter.py
+++ kerter.py
@@ -1,7 +1,11 @@
-
 def nyolcszogKerulet():
 		a=float(input("Kérlek add meg a nyolcszög oldalát [cm]: "))
 		return 8*a
+def nyolcszogTerulet():
+		a=float(input("Kérlek add meg a nyolcszög oldalát [cm]: "))
+		r=float(input("Kérlek add meg a nyolcszög sugarát [cm]: "))
+		return 4*a*r
+
 
 print("1-háromszög, 2-kör, 3-téglalap, 4-nyolcszág")
 
